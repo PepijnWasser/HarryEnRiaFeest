45e2692e66fba7c5da214aceb8980437a339e661
Pepijn Wasser <60891244+PepijnWasser@users.noreply.github.com>
2023-11-26 10:44:25 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,8 +3,17 @@ document.querySelector("button").onclick = function() {
   document.querySelector("#loading").style.display = "none"
 }
 
-var button2Element = document.getElementById("button2").onclick = function() {
-  window.location.href = 'website.html';
-  document.querySelector("#loading").style.display = "none"
-}
+var button2Element = document.getElementById("button2");
 
+// Controleer of het element is gevonden
+if (button2Element) {
+    // Voeg een onclick-eventhandler toe
+    button2Element.onclick = function() {
+        alert("Button 2 is geklikt!");
+        // Voeg hier de gewenste functionaliteit toe
+          window.location.href = 'website.html';
+          document.querySelector("#loading").style.display = "none"
+    };
+} else {
+    console.log("Button 2 niet gevonden");
+}
