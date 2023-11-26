c9fe11d2631eacf084e3faccd0a68951f331704e
Pepijn Wasser <60891244+PepijnWasser@users.noreply.github.com>
2023-11-26 10:54:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ document.querySelector("button").onclick = function() {
   window.location.href = 'website.html';
   document.querySelector("#loading").style.display = "none"
 }
+
+// Zoek het knopelement met id "button2"
+var button2Element = document.getElementById("button2");
+console.log
+// Controleer of het element is gevonden
+if (button2Element) {
+    // Voeg een gebeurtenisluisteraar toe voor het klikken van de knop
+    button2Element.addEventListener("click", function() {
+        // Verander de locatie naar de gewenste pagina
+        window.location.href = "nieuwe_pagina.html";
+    });
+} else {
+    // Melding als het element niet is gevonden
+    console.log("Element met id 'button2' niet gevonden.");
+}
