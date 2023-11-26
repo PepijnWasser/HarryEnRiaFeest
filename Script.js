e9ec6a9c45f3a231bb5d071690d44e8e81cbae3c
Pepijn Wasser <60891244+PepijnWasser@users.noreply.github.com>
2023-11-26 10:34:45 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+document.querySelector("button").onclick = function() {
+  window.location.href = 'website.html';
+  document.querySelector("#loading").style.display = "none"
+}
+
+document.querySelector("button2").onclick = function() {
+  window.location.href = 'website.html';
+  document.querySelector("#loading").style.display = "none"
+}
